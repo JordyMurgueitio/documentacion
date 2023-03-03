e7ac7bc088d00253077309b8d7121790e3bda59a
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-02 21:41:08 -0500

    adding more functions

--- javascript/cheatsheets/functions.js
+++ javascript/cheatsheets/functions.js
@@ -7,5 +7,21 @@ function greet() {
 }
 
 
-// 2. Calling a function 
+// 2. Calling a function - The code inside a function body runs, or executes, only when the function is called.
+function sayThanks () {
+    console.log("Thanks for comming ");
+}
 
+sayThanks();
+sayThanks();
+
+
+// 3. Parameters and arguments - allow functions to accept input(s) and perform a task using the input(s).  
+function sayGracias(name) {
+    console.log(`thanks for buying, ${name}`);
+}
+
+sayGracias("Erik");
+
+
+// 4. default parameters - allow parameters to have a predetermined value in case there is no argument passed into the function when called.
