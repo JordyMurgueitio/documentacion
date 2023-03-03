75cb39bd41a637e4813ba712a3d6fccafeef8592
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-02 21:26:11 -0500

    adding functions

---
+++ javascript/cheatsheets/functions.js
@@ -0,0 +1,11 @@
+// FUNCTIONS 
+
+
+// 1. Function declarations - binds a function to a name, or an identifier
+function greet() {
+    console.log("Hola amigo");
+}
+
+
+// 2. Calling a function 
+
