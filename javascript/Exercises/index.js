592324a60713a09537c60bb40a48d9e5fad60fab
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-07 09:40:02 -0500

    adding example

---
+++ javascript/Exercises/index.js
@@ -0,0 +1,9 @@
+const getCalories = (weight, height) => weight * height;
+
+console.log(getCalories(20,2));
+
+let number1 = 230;
+let number2 = 330;
+
+number1 > number2 ? console.log(`the bigger is ${number1}`)
+: console.log(`the bigger is ${number2}`);
