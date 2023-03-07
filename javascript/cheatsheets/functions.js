4666dfb35b968afb75a1d24c9db6906281a0d324
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-07 08:24:36 -0500

    finishing functions

--- javascript/cheatsheets/functions.js
+++ javascript/cheatsheets/functions.js
@@ -1,13 +1,13 @@
-// FUNCTIONS 
+// FUNCTIONS  - reusable block of code that groups together a sequence of statements to perform a specific task.
 
 
-// 1. Function declarations - binds a function to a name, or an identifier
+// 1. FUNCTION DECLARATION - binds a function to a name, or an identifier
 function greet() {
     console.log("Hola amigo");
 }
 
 
-// 2. Calling a function - The code inside a function body runs, or executes, only when the function is called.
+// 2. CALLING A FUNCTION - The code inside a function body runs, or executes, only when the function is called.
 function sayThanks () {
     console.log("Thanks for comming ");
 }
@@ -16,7 +16,7 @@ sayThanks();
 sayThanks();
 
 
-// 3. Parameters and arguments - allow functions to accept input(s) and perform a task using the input(s).  
+// 3. PARAMETERS AND ARGUMENTS - allow functions to accept input(s) and perform a task using the input(s).  
 function sayGracias(name) {
     console.log(`thanks for buying, ${name}`);
 }
@@ -24,7 +24,7 @@ function sayGracias(name) {
 sayGracias("Erik");
 
 
-// 4. default parameters - allow parameters to have a predetermined value in case there is no argument passed into the function when called.
+// 4. DEFAULT PARAMETERS - allow parameters to have a predetermined value in case there is no argument passed into the function when called.
 function greeting (name = 'stranger') {
     console.log(`Hello, ${name}!`)
 }
@@ -33,7 +33,7 @@ greeting('Nick') // Output: Hello, Nick!
 greeting() // Output: Hello, stranger!
 
 
-// 5. return - allows functions to produce an output. We can then save the output to a variable for later use
+// 5. RETURN - allows functions to produce an output. We can then save the output to a variable for later use
 function monitorCount(rows, columns) {
     return rows * columns;
 }
@@ -41,7 +41,7 @@ const numOfMonitors = monitorCount(5, 4);
 console.log(numOfMonitors);
 
 
-// 6. Helper Function - functions being called within another function
+// 6. HELPER FUNCTION - functions being called within another function
 function multiplyByNineFifths(number) {
     return number * (9/5);
 }
@@ -52,5 +52,39 @@ function fahrenheit (celsius) {
 fahrenheit(26);
 
 
-// 7. Function expressions - the function name is usually omitted. - often stored in a variable in order to refer to it.
+// 7. FUNCTION EXPRESSIONS - the function name is usually omitted. often stored in a variable in order to refer to it. 
+//To invoke a function expression, write the name of the variable in which the function is stored
+const plantNeedsWater = function (day) {
+    if (day == "Wednesday") {
+        return true;
+    } else {
+        return false;
+    }
+};
+console.log(plantNeedsWater("Wednesday"));
+
+
+// 8. ARROW FUNCTIONS  - shorter way to write functions by using the special “fat arrow” () => notation.
+// remove the need to type out the keyword function. you first include the parameters inside the ( ) and then add an arrow =>
+const rectArea = (width, height) => { 
+    let area = width * height; 
+    return area;
+}
+console.log(rectArea(10, 10));
+
+
+// 9. CONCISE BODY ARROW FUNCTIONS - several ways to refactor arrow function syntax. most condensed form of the function is concise body
+
+    // 9.1. 
+    const hello = () => {};  //function takes zero or multiple parameters, parentheses are required.
+    const welcome = parameter => {}; //Functions that take only a single parameter do not need that parameter to be enclosed in parentheses
+    const sayHi = (name1, name2) => {};  // 2 or more parameters, parentheses are required
+
+    // 9.2.
+    // function body composed of a single-line block does not need curly braces the return keyword can be removed. This is referred to as implicit return.
+    /* const otherSum = numero => {
+        let sum = numero + numero;
+        return sum;
+    }; */
+    const otherSum = numero => numero + numero;  
 
