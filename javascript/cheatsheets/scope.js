0c8e4874e9351d444a15874338e65a9760837447
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-08 17:58:05 -0500

    adding global scope

--- javascript/cheatsheets/scope.js
+++ javascript/cheatsheets/scope.js
@@ -11,3 +11,16 @@ function logCitySkyline () {
 }
 console.log(logCitySkyline()); // function is able to access both variables without any problems.
 
+
+// 2. GLOBAL SCOPE - variables are declared outside of blocks. These variables are called global variables.
+// they can be accessed by any code in the program, including code in blocks.
+
+let satellite = "The Moon"; 
+let galaxy = "The Milky Way";
+let stars = "North Star";
+
+function callMyNightSky () {
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
