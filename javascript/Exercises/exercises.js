54d0548106132a1fff40afa463036d5a5e1c8103
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-12 18:29:07 -0500

    adding exercise

--- javascript/Exercises/exercises.js
+++ javascript/Exercises/exercises.js
@@ -129,3 +129,32 @@ function truthyOrFalsy(data) {
     }
 };
 
+
+// 8. Function takes a number of shared dna and returns the relationship
+const whatRelation = percentSharedDNA => {
+    if (percentSharedDNA === 100) {
+        return 'You are likely identical twins.'
+    }
+    if (percentSharedDNA >= 35 && percentSharedDNA < 100) {
+        return 'You are likely parent and child or full siblings.'
+    }
+    if (percentSharedDNA >= 14 && percentSharedDNA < 35) {
+        return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+    }
+    if (percentSharedDNA >= 6 && percentSharedDNA < 14) {
+        return 'You are likely 1st cousins.'
+    }
+    if (percentSharedDNA >= 3 && percentSharedDNA < 6) {
+        return 'You are likely 2nd cousins.'
+    }
+    if (percentSharedDNA >= 1 && percentSharedDNA < 3) {
+        return 'You are likely 3rd cousins'
+    }
+    return 'You are likely not related.'
+}
+
+console.log(whatRelation(34)); // Should print 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+
+console.log(whatRelation(3)); // Should print 'You are likely 2nd cousins.'
+
+
