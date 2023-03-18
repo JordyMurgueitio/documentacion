a847519f9b7ac6079afffd92f3416c0c96745a49
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-18 07:20:24 -0500

    adding for loop

--- javascript/loops/loops.js
+++ javascript/loops/loops.js
@@ -1,1 +1,18 @@
 // LOOPS - repeats a set of instructions until a specified condition, called a stopping condition is reached
+
+
+
+// 1. The For loop -  The iterator variable is initialized, checked against the stopping condition, and assigned a new value on each loop iteration
+
+// initialization is let count = 5, so the loop will start counting at 5.
+// stopping condition is count < 11, the loop will run as long as the iterator variable, count, is less than 4.
+// iteration statement is count++. after each loop, the value of count will increase by 1
+
+for (let count = 5; count < 11; count++) {
+    console.log(count);  // logs 5, 6, 7, 8, 9, 10
+}
+
+
+
+// 2. Looping in reverse 
+
