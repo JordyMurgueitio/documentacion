c2fd97cf501a96111697bcff4f0b44bf4ec3eee9
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-21 13:28:22 -0500

    adding objects file

---
+++ javascript/objects/objects.js
@@ -0,0 +1,14 @@
+// OBJECTS - containers storing related data and functionality
+
+
+
+// 1. Object literals -  use curly braces, {}, to designate an object literal:
+// // This data is organized into key-value pairs. 
+// A key’s value can be of any data type including functions or other objects.
+
+let spaceShip = {
+    'fuel type': 'diesel', 
+    color: 'silver'
+}
+
+console.log(spaceShip);
