9d2fc33299f69bd176ff833805108403a50dd855
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-23 08:17:54 -0500

    adding object properties

--- javascript/objects/objects.js
+++ javascript/objects/objects.js
@@ -14,4 +14,26 @@ console.log(spaceShip);
 
 
 
-// 2. Accessing Properties - 
+// 2. Accessing Properties - write the object’s name, followed by the dot operator and then the property name (key)
+const car = {
+    color: 'red', 
+    model: 'ford',
+    year: 2020
+}
+let carColor = car.color;
+console.log(carColor);  // logs red
+
+// 2.1 - second way to access a key’s value is by using bracket notation, [ ]
+// we pass in the property name (key) as a string.
+// We must use bracket notation when accessing keys that have numbers, spaces, or special characters in them.
+let spaceship = {
+    'Fuel Type': 'Turbo Fuel',
+    'Active Duty': true,
+    homePlanet: 'Earth',
+    numCrew: 5
+};
+console.log(spaceship['Active Duty']);  // logs true
+
+// you can also use a variable inside the brackets to select the keys of an object. This can be especially helpful when working with functions:
+let returnAnyProp = (objectName, propName) => objectName[propName];
+returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
