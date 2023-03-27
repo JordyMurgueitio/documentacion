bb5711f9927df8a90fa7c01d9492837c3a2b89f3
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-26 19:15:00 -0500

    finishing advanced objects

--- javascript/objects/advanced-objects.js
+++ javascript/objects/advanced-objects.js
@@ -101,6 +101,93 @@ car1.start();
 
 
 
-// 6. Property value shorthand - 
+// 6. Property value shorthand -  When the key name was the same as the parameter name we assigned to it. shortcuts for assigning properties to variables
+// we don’t have to repeat ourselves for property assignments
+
+const userMaker = (name, age) => {
+    return {
+        name, 
+        age
+    }
+}
+const testUser = userMaker("jordy", 24);  
 
 
+
+
+// 7. Destructured Assignment - extract key-value pairs from objects and save them as variables
+// we create a variable with the name of an object’s key that is wrapped in curly braces { } and assign to it the object
+
+const vampire = {
+    name: 'dracula', 
+    residence: 'transylvania', 
+    preferences: {
+        day: "stay inside", 
+        night: "satisfy appetite"
+    }
+};
+const {residence} = vampire;  // we declare a new variable residence that extracts the value of the residence property of vampire
+console.log(residence);  // logs transylvania 
+const {day} = vampire.preferences;   // destructured assignment to grab nested properties of an object
+console.log(day);  // logs stay inside
+
+
+const robot123 = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+        beep() {
+            console.log('Beep Boop');
+        },
+        fireLaser() {
+            console.log('Pew Pew');
+        },
+        }
+};
+// Since functionality is referencing robot.functionality we can call the methods available to robot.functionality simply through functionality
+const {functionality} = robot123;  
+functionality.beep();   
+
+
+
+
+
+// 8. Built-in object methods 
+
+// 8.1 Object.keys() - returns an array of a given object's own enumerable string-keyed property names
+// Object.keys(obj)
+const computer = {
+    model: 'apple', 
+    color: 'white'
+}
+const computerKeys = Object.keys(computer);
+console.log(computerKeys); // logs ["model", "color"]
+
+// 8.2 Object.entries() - return an array that contain more arrays that have both the key and value of the properties in an object
+// Object.entries(obj)
+const computerEntries = Object.entries(robot);
+console.log(computerEntries);
+
+// 8.3 Object.assign() - copies all enumerable own properties from one object to a target object. It returns the modified target object
+// Object.assign(target, ...sources)
+const newComputer = Object.assign({year: 2023, camera: true}, computer); 
+console.log(newComputer);
+
+
+
+
+
+
+// REVIEW
+/* 
+1. The object that a method belongs to is called the calling object.
+2. The this keyword refers to the calling object and can be used to access properties of the calling object.
+3. Methods do not automatically have access to other internal properties of the calling object.
+4. The value of this depends on where the this is being accessed from.
+5. We cannot use arrow functions as methods if we want to access other internal properties.
+6. JavaScript objects do not have built-in privacy, rather there are conventions to follow to notify other developers about the intent of the code.
+7. The usage of an underscore before a property name means that the original developer did not intend for that property to be directly changed.
+8. Setters and getter methods allow for more detailed ways of accessing and assigning properties.
+9. Factory functions allow us to create object instances quickly and repeatedly.
+10. There are different ways to use object destructuring: one way is the property value shorthand and another is destructured assignment.
+*/
