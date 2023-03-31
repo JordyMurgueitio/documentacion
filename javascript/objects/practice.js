b19f566fe48b6715a15c7e5d41182d673f06a881
Jordy Murgueitio <jordinho.15@gmail.com>
2023-03-30 22:07:04 -0500

    adding object exercises

--- javascript/objects/practice.js
+++ javascript/objects/practice.js
@@ -44,3 +44,41 @@ console.log(model); // logs ford
 
 
 
+//  3. Team stats
+
+const team = {
+    _players: [{firstName: 'leo', lastName: 'messi', age: 34}, 
+                {firstName: 'eden', lastName: 'hazard', age: 32}, 
+                {firstName: 'niño', lastName: 'moi', age: 22} ], 
+    
+    _games: [{opponent: 'barca', teamPoints: 3, opponentPoints: 0}, 
+            {opponent: 'arsenal', teamPoints: 0, opponentPoints: 0},
+            {opponent: 'barca', teamPoints: 3, opponentPoints: 0}], 
+    get players () {
+        return this._players;
+    }, 
+    get games () {
+        return this._games;
+    }, 
+    addPlayer (newFirstName, newLastName, newAge) {
+        const player = {
+            firstName: newFirstName,
+            lastName: newLastName, 
+            age: newAge
+        }
+        this._players.push(player);
+    },
+    addGame (newOpponent, newTeamPoints, newOpponentPoints) {
+        const game = {
+            opponent: newOpponent,
+            teamPoints: newTeamPoints, 
+            opponentPoints: newOpponentPoints
+        }
+        this._games.push(game);
+    }
+}
+
+team.addGame('liga', 0, 3);
+team.addPlayer('jordy', 'murgueitio', 24);
+console.log(team._players);
+console.log(team._games);
