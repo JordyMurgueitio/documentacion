b50f158da72dbc8a558a8a8d85226ea39e7427ec
Jordy Murgueitio <jordinho.15@gmail.com>
2023-04-16 20:03:29 -0500

    adding map method

--- javascript/iterators/iterators.js
+++ javascript/iterators/iterators.js
@@ -2,7 +2,7 @@
 
 
 
-// 1. .forEach() method - will execute the same code for each element of an array.
+// 1. the .forEach() method - will execute the same code for each element of an array.
 // loops through the array and executes the callback function for each element. 
 // During each execution, the current element is passed as an argument to the callback function.
 
@@ -25,3 +25,18 @@ const printSport = element => {
     console.log(`My favorite sport is ${element}`);
 }
 sports.forEach(printSport);
+
+
+
+
+// 2. the .map() method - it takes an argument of a callback function and returns a new array
+// .map will iterate through each element in the array and pass the element into the callback function.
+
+const numbers = [1, 2, 3, 4, 5]; 
+
+const bigNumbers = numbers.map(number => {
+    return number * 10;
+});
+
+console.log(numbers); // logs [1, 2, 3, 4, 5]
+console.log(bigNumbers);  // logs [10, 20, 30, 40, 50]
