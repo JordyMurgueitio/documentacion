d307f54d54e8bab4ae3f75cd079b4b2f9cdb25ed
Jordy Murgueitio <jordinho.15@gmail.com>
2023-04-17 08:15:49 -0500

    adding reduce method

--- javascript/iterators/iterators.js
+++ javascript/iterators/iterators.js
@@ -88,4 +88,36 @@ console.log(biggerThan10);  // logs 3
 
 
 
-// 
+// 5. The .reduce() method -  returns a single value after iterating through the elements of an array, thereby reducing the array
+//the return value of the callback function becomes the accumulator value for the next iteration, 
+// currentValue takes on the value of the current element in the looping process.
+
+/* Iteration	accumulator	currentValue	return value
+First	        1	         3	              4 
+Second	        4	         5	              9
+Third	        9	         7	              16              */
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+    console.log(`The value of accumulator: ${accumulator}`);
+    console.log(`The value of currentValue: ${currentValue}`);
+    return accumulator + currentValue;
+});
+console.log(newSum);  // returns 16
+
+
+//The .reduce() method can also take an optional second parameter to set an initial value for accumulator
+
+const otherNumbers = [1, 3, 5, 7];
+
+const suma = otherNumbers.reduce((accumulator, currentValue) => {
+    console.log(`The value of accumulator: ${accumulator}`);
+    console.log(`The value of currentValue: ${currentValue}`);
+    return accumulator + currentValue;
+}, 10);  // 10 is the second parameter that sets the initial value for accumulator
+console.log(suma);  // returns 26
+
+
+
+
