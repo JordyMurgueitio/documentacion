47f9438e527d2005cce6e1193c8cccd977a54081
Jordy Murgueitio <jordinho.15@gmail.com>
2023-04-25 10:54:36 -0500

    adding exercise

--- javascript/exercises/18-04-23.js
+++ javascript/exercises/18-04-23.js
@@ -120,3 +120,42 @@ const justCoolStuff = (firstArray, secondArray) => {
 const objetos2 = ["hola" ,"hdi", "ds"]
 const objetos1 = ["hola" ,"hddi", "ddfs"]
 console.log(justCoolStuff(objetos1, objetos2)); // logs ["hola"]
+
+
+// 9. Write a function, dogFactory(). It should:
+// have 3 parameters: name, breed, and weight (in that order)
+// expect name and breed to be strings
+// expect weight to be a number
+// return an object
+
+const dogFactory = (name, breed, weight) => {
+    return {
+        _name: name,
+        _breed: breed,
+        _weight: weight,
+        get name() {
+            return this._name;
+        },
+        set name(newName) {
+            this._name = newName;
+        },
+        get breed() {
+            return this._breed;
+        },
+        set breed(newBreed) {
+            this._breed = newBreed;
+        },
+        get weight() {
+            return this._weight;
+        },
+        set weight(newWeight) {
+            this._weight = newWeight;
+        },
+        bark() {
+            return 'ruff! ruff!'
+        },
+        eatTooManyTreats() {
+            this._weight++
+        }
+    }
+}
