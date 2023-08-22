e420ef2e9ab08c2293efd9b4b7cbb40a68c0139b
Jordy Murgueitio <jordinho.15@gmail.com>
2023-08-22 13:42:58 -0500

    adding info

---
+++ javascript/destructuring/destructuring.js
@@ -0,0 +1,13 @@
+// DESTRUCTURING - is a JavaScript feature that makes it easier to extract data from arrays and objects
+
+
+
+// 1. Destructuring Arrays
+
+/* If we wanted to access these cars individually and assign them to variables 
+We can use destructuring to shorten our code and make it more concise:*/
+let cars = ['ferrari', 'tesla', 'cadillac'];
+let [car1, car2, car3] = cars; 
+
+console.log(car1, car2, car3);  // Prints: ferrari tesla cadillac
+
