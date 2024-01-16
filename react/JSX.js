1a38b12a0d8956e78b57a290a4b6b9ad48b15339
Jordy Murgueitio <jordinho.15@gmail.com>
2024-01-16 18:38:54 -0500

    adding info jsx

---
+++ react/JSX.js
@@ -0,0 +1,18 @@
+//  JSX -is a syntax extension for JavaScript. It was written to be used with React. 
+
+
+// 1. JSX ELEMENTS  - A basic unit of JSX is called a JSX element.
+
+<h1>Hello world</h1>
+// a JSX element can be saved in a variable, passed to a function, stored in an object or array…
+const navBar = <nav>I am a nav bar</nav>
+
+
+
+// 2. Attributes in JSX - JSX elements can have attributes, just like how HTML elements can.
+
+const title = <h1 id="title">Into to React</h1>
+
+
+
+// 3. Nested JSX 
