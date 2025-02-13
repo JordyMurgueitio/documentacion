a7fb792ee7dc09b383ae94b818683f7bd43e4546
Jordy Murgueitio <jordinho.15@gmail.com>
2025-02-13 09:13:57 -0500

    adding info

--- javascript/SYNTAX/Conditionals/conditionals.js
+++ javascript/SYNTAX/Conditionals/conditionals.js
@@ -158,4 +158,25 @@ switch (arrivedFrom) {      // ( ... ),  contains the value that each case will 
 
 
 
-
+const lifePhase = age => {
+    switch(true) {
+        case (age >= 0 && age < 4):
+            return 'baby';
+            break;
+        case (age >= 4 && age < 13):
+            return 'child';
+            break;
+        case (age >= 13 && age < 20):
+            return 'teen';
+            break;
+        case (age >= 20 && age < 65):
+            return 'adult';
+            break;
+        case (age >= 65 && age <= 140):
+            return 'senior citizen';
+            break;
+        default:
+            return 'This is not a valid age';
+            break;
+    };
+};
