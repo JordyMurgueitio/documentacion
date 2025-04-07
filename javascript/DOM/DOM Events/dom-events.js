6ccda8564d6b44d2b465a54db3e7590d434d2e96
Jordy Murgueitio <jordinho.15@gmail.com>
2025-04-07 18:50:28 -0500

    adding info

--- javascript/DOM/DOM Events/dom-events.js
+++ javascript/DOM/DOM Events/dom-events.js
@@ -5,7 +5,7 @@
 
 
 // Registering Event handlers
-// Functions that modify and update DOM elements after an event fires
+// Event Handlers: Functions that modify and update DOM elements after an event fires
 
 // .addEventListener() method -  
 // we can have a DOM element listen for a specific event and execute a block of code when the event is detected. 
@@ -37,56 +37,52 @@ However, with the .addEventListener() method , we can add multiple event handler
 
 
 // Removing event handlers 
-// The .removeEventListener() method is used to reverse the .addEventListener() method
-/* .removeEventListener() also takes two arguments:
-    1. The event type as a string
-    2. the name of the event handler you want to remove
-*/
 
+// .removeEventListener() method -
+// Used to reverse the .addEventListener() method
+/* Takes two arguments:
+    1. The event type as a string
+    2. the name of the event handler you want to remove  */
 eventTarget.removeEventListener("click", eventHandlerFunction);
 // If .addEventListener() was provided an anonymous function, then that event listener cannot be removed
-
-let fortunes = ["A beautiful, smart, and loving person will be coming into your life.",
-    "A fresh start will put you on your way.",
-    "A golden egg of opportunity falls into your lap this month.",
-    "A smile is your personal welcome mat.",
-    "All your hard work will soon pay off."];
-let button = document.getElementById('fortuneButton');
-let fortune = document.getElementById('fortune');
-
-function fortuneSelector(){
-    let randomFortune = Math.floor(Math.random() * fortunes.length);
-        return fortunes[randomFortune];
-}
+// Make sure the eventHandlerFunction in the removeEventListener() method is the same function of the corresponding addEventListener() method 
 function showFortune(){
-    fortune.innerHTML = fortuneSelector();
     button.innerHTML = "Come back tomorrow!";
     button.style.cursor = "default";
     button.removeEventListener("click", showFortune);
 }
-
 button.addEventListener('click', showFortune);
 
 
 
 
-// 3. Event Object properties -  pre-determined properties associated with event objects. 
-// call these properties to see information about the event
+// Event Object properties -  
+// JavaScript stores events as Event objects with their related data and functionalities as properties and methods
+// The event object is passed as an argument to the event handler function when the event is triggered  
+function eventHandlerFunction(event){
+    console.log(event.timeStamp);
+}
+eventTarget.addEventListener('click', eventHandlerFunction); /* we log the time it took for the event to be triggered 
+since the document was loaded by accessing the .timeStamp property of the event object */
 
-    // 3.1  .target property to reference the element that the event is registered to the event .target is a DOM element and you can access
-    // 3.2  .type property to access the name of the event.
-    // 3.3 .timeStamp property to access the number of milliseconds that passed since the document loaded and the event was triggered.
+/* There are pre-determined properties associated with event objects. 
+You can call these properties to see information about the event */
+/* 
+.target property reference the element that the event is registered to.
+.type property to access the name of the event.
+.timeStamp property to access the number of milliseconds that passed since the document loaded and the event was triggered. */
+let share = document.getElementById('share-button');
+let text = document.getElementById('text');
 function sharePhoto (event) {
     event.target.style.display = "none";
     text.innerHTML = event.timeStamp; 
 }; 
-
 share.addEventListener("click", sharePhoto);
 
 
 
 
-// 4. Event types 
+// Event types 
 
     // 4.1 Mouse Events - click, wheel, mousedown, mouseup, mouseover, mouseout
 
