04ed7f8c45fc76493f5313b9e38ee531fc8f7e80
Jordy Murgueitio <jordinho.15@gmail.com>
2025-05-01 09:38:40 -0500

    adding classes

--- javascript/SYNTAX/Classes/classes.js
+++ javascript/SYNTAX/Classes/classes.js
@@ -23,7 +23,6 @@ const tobi = new Dog("tobi");
 console.log(tobi);
 
 
-
 // 2. Constructor - 
 // JavaScript calls the constructor() method every time it creates a new instance of a class
 // Inside of the constructor() method, we use the this keyword. In the context of a class, this refers to an instance of that class
@@ -171,3 +170,77 @@ We use the extends keyword to create a subclass.
 The super keyword calls the constructor() of a parent class.
 Static methods are called on the class, but not on instances of the class. */
 
+
+
+class Media {
+    constructor (title) {
+        this._title = title; 
+        this._isCheckedOut = false; 
+        this._ratings = [];
+    }
+    set isCheckedOut (newValue) {
+        this._isCheckedOut = newValue;
+    }
+    get title () {
+        return this._title;
+    }
+    get isCheckedOut () {
+        return this._isCheckedOut;
+    }
+    get ratings () {
+        return this._ratings;
+    }
+    getAverageRating() {
+        let ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+        const lengthOfArray = this._ratings.length;
+        return ratingsSum / lengthOfArray;
+    }
+    toggleCheckOutStatus() {
+        this._isCheckedOut = !this._isCheckedOut;
+    }
+    addRating (value) {
+        this._ratings.push(value);
+    }
+};
+
+class Book extends Media {
+    constructor (author, title, pages) {
+        super(title); 
+        this._author = author; 
+        this._pages = pages;
+    }
+    get author () {
+        return this._author; 
+    }
+    get pages () {
+        return this._pages;
+    }
+};
+
+class Movie extends Media {
+    constructor (director, title, runTime) {
+        super(title); 
+        this._director = director; 
+        this._runTime = runTime;
+    }
+    get director () {
+        return this._director; 
+    }
+    get runTime () {
+        return this._runTime;
+    }
+};
+
+const historyOfEverything = new Book("Bill Bryson", 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+console.log(speed.getAverageRating());
