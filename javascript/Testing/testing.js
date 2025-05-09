50b55ed8cdc759fb6bb71ec1843e144b8ace14bd
Jordy Murgueitio <jordinho.15@gmail.com>
2025-05-09 07:13:22 -0500

    adding testing

---
+++ javascript/Testing/testing.js
@@ -0,0 +1,63 @@
+// Automated Testing 
+
+/* Automated testing is the use of software to control the execution of tests 
+and the comparison of actual behavior to expected behavior */
+
+
+
+// The test Suite
+// A test suite is a collection of test cases that are intended to be used together.
+// You can refer to the code defining your app as implementation code, and the code defining your tests as test code
+
+
+
+// Regression 
+// When an existing feature stops working after a change has been made to the code, it is called a regression.
+// Regression testing is the process of testing existing software applications to make sure that a change or bug fix has not broken any existing functionality
+
+
+
+// Unit Testing
+// A unit test covers the smallest possible unit of testable code
+// Unit tests would be written first to ensure that these functions can perform independently before we move on to testing how they work together. 
+// Any data that might come in from an external database or API would be mocked.
+
+
+
+// Integration Testing
+// An integration test covers how the units of a particular program work with one another.
+
+
+
+// End to End testing
+// An end-to-end test covers the entire application from start to finish.
+// Automates user flow to test the application in the way that a real user would experience it
+
+
+
+
+// What is a software testing methodology?
+// A software testing methodology is a set of principles and practices that guide the testing process.
+
+// Types of testing methodologies - writing test cases before writing the code
+
+// 1. Test Driven Development (TDD)
+// tests are written before the functioning code is written
+// TDD is a software development process that relies on the repetition of a very short development cycle:
+    // 1. Write a failing automated test case that defines a desired improvement or new function
+    // 2. Write the minimum amount of code necessary to pass that test
+    // 3. Refactor the new code to acceptable standards
+    // 4. Repeat
+
+
+// 2. Behavior Driven Development (BDD)
+// BDD is an extension of TDD that focuses on the behavior of the application from the end user's perspective.
+// BDD encourages collaboration between developers, QA, and non-technical or business participants in a software project.
+// BDD is a software development process that relies on the repetition of a very short development cycle:
+    // 1. Write a failing automated test case that defines a desired improvement or new function
+    // 2. Write the minimum amount of code necessary to pass that test
+    // 3. Refactor the new code to acceptable standards
+    // 4. Repeat
+
+
+
